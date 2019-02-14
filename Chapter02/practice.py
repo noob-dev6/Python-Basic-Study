ddb5050c3b2a845626c403abc2345e760789227a
rnsjtngus <noob.dev6@gmail.com>
2019-02-14 21:54:39 +0900

    Chapter 2 연습문제 추가

---
+++ Chapter02/practice.py
@@ -0,0 +1,3 @@
+seconds_per_hour = 60 * 60
+seconds_per_day = 24 * seconds_per_hour
+print(seconds_per_day, seconds_per_hour, seconds_per_day / seconds_per_hour, seconds_per_day // seconds_per_hour)
